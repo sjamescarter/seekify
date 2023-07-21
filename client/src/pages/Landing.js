6527da5f521f87201ad4afbeae5ed493e74c06ea
sjamescarter <stephenjamescarter@gmail.com>
2023-07-20 22:39:03 -0500

    create Landing Page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function Landing() {
+    return (
+        <div>
+            <h1>This is the landing page</h1>
+            <p>You can find out about us.</p>
+            <p>And sign in or sign up</p>
+        </div>
+    )
+}
+
+export default Landing;
