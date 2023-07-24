cb8d2204efa6bab2c9edc47959dd7738e0e6abb5
sjamescarter <stephenjamescarter@gmail.com>
2023-07-24 09:39:05 -0500

    update styling add placeholders

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,36 @@
-function NavBar() {
+import { useContext } from 'react';
+import styled from 'styled-components';
+import { UserContext } from "../context/user";
+
+function NavBar({ children }) {
+    const { user, setUser } = useContext(UserContext);
+
     return (
-        <h1>NavBar</h1>
+        <Main>
+            <NavGrid>
+                <h1>seekify.io</h1>
+                <input type='text' placeholder='Search Bar' />
+                <button>menu</button>
+            </NavGrid>
+        </Main>
     );
 }
+
+const Main = styled.div`
+    background-color: #8AA29E;
+    color: white;
+    position: fixed;
+    top: 0;
+    width: 100%;
+`
+
+const NavGrid = styled.div`
+    align-items: center;
+    display: grid;
+    grid-template-columns: 3fr 2fr 1fr;
+    padding: 10px;
+    max-width: 800px;
+    margin: auto;
+`
 
 export default NavBar;
