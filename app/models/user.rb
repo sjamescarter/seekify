f1200b1e96e5602094bc38bedf15b67b24cd8894
sjamescarter <stephenjamescarter@gmail.com>
2023-07-29 16:01:10 -0500

    add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_one :profile, dependent: :destroy
 
   validates :email, 
     format: { with: /\A[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}\z/}, 
