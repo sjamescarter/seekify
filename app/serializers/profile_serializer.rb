af420a8b750b48cb97d18bbbed67403a484558f6
sjamescarter <stephenjamescarter@gmail.com>
2023-07-31 15:24:12 -0500

    add name method

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,11 +1,15 @@
 class ProfileSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :avatar, :first_name, :last_name, :phone, :city, :state, :bio, :video_url
+  attributes :id, :avatar, :name, :phone, :city, :state, :bio, :video_url
   has_one :user
   has_one :venue
 
   def avatar
     url_for(object.avatar) if object.avatar.attached?
+  end
+
+  def name 
+    "#{object.first_name} #{object.last_name}"
   end
 end
