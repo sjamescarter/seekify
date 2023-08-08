e5099a65b629dbbdd9838fbc583c560cd85131dc
sjamescarter <stephenjamescarter@gmail.com>
2023-08-07 22:43:44 -0500

    update phone validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,7 @@ class Profile < ApplicationRecord
   validates :avatar, content_type: ['image/gif', 'image/jpg', 'image/jpeg', 'image/png']
   validates :first_name, :last_name, :city, presence: true, format: { without: /[0-9]/ }
   validates :bio, length: { maximum: 1000 }
-  validates :phone, length: { maximum: 12 }, format: { with: /[0-9]/}, allow_blank: true
+  validates :phone, length: { is: 12 }, format: { with: /[0-9]/}, allow_blank: true
   validates :state, inclusion: { in: %w(AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY)}
   validates :video_url, url: { allow_blank: true}
 end
