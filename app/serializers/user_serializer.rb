e19b09dd0ddccf9e03a1e3ac8a788b53093d23cb
sjamescarter <stephenjamescarter@gmail.com>
2023-08-09 16:30:18 -0500

    add name method

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email
+  attributes :id, :email, :name
+  has_many :user_instruments
   has_one :profile
   has_one :church
+
+  def name
+    return if object.profile.nil?
+    "#{object.profile.first_name} #{object.profile.last_name}"
+  end
 end
