73e1efa668e905601c337a8f30b403be7207474b
sjamescarter <stephenjamescarter@gmail.com>
2023-08-09 16:29:06 -0500

    add validations

--- app/models/user_instrument.rb
+++ app/models/user_instrument.rb
@@ -1,4 +1,11 @@
 class UserInstrument < ApplicationRecord
   belongs_to :user
   belongs_to :instrument
+
+  validates :skill, 
+    presence: true, 
+    inclusion: { in: %w(beginner intermediate advanced professional)}
+  validates :experience, 
+    presence: true, 
+    numericality: { only_integer: true, in: 0..95 }
 end
