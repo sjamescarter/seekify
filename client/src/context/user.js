751d7b8ccaef8d57b922248deb349ddc68acb9cc
sjamescarter <stephenjamescarter@gmail.com>
2023-08-08 21:04:46 -0500

    update with instrument state

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -5,9 +5,10 @@ const UserContext = createContext("");
 function UserProvider({children}){
     const [user, setUser] = useState();
     const [venues, setVenues] = useState();
-    
+    const [instruments, setInstruments] = useState();
+
     return (
-        <UserContext.Provider value={{ user, setUser, venues, setVenues }}>
+        <UserContext.Provider value={{ user, setUser, venues, setVenues, instruments, setInstruments }}>
             {children}
         </UserContext.Provider>
     );
