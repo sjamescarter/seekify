4d2235dd36625988bd30985995f40da94132784f
sjamescarter <stephenjamescarter@gmail.com>
2023-08-09 16:27:09 -0500

    refactor create action, add destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create(user_params)
-    if user.valid?
-      session[:user_id] = user.id
-      render json: user, status: :created
-    else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
   end
 
   def show
     render json: @current_user
+  end
+
+  def destroy
+    @current_user.destroy
+    head :no_content
   end
 
   private
