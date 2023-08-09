993ef709ab14d6178575517041092a3ac333b14b
sjamescarter <stephenjamescarter@gmail.com>
2023-08-09 16:27:52 -0500

    add has_many associations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,7 @@
 class Instrument < ApplicationRecord
+  has_many :user_instruments, dependent: :destroy
+  has_many :users, through: :user_instruments
+
   validates :name, 
     presence: true, 
     format: { without: /[0-9]/ }, 
