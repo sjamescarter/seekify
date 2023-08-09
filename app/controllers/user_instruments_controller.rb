78207da6ed5e9a8eb498fb3d7e5676cf374469ab
sjamescarter <stephenjamescarter@gmail.com>
2023-08-09 16:26:13 -0500

    add index and create actions

--- app/controllers/user_instruments_controller.rb
+++ app/controllers/user_instruments_controller.rb
@@ -1,2 +1,18 @@
 class UserInstrumentsController < ApplicationController
+  def index
+    render json: UserInstrument.all
+  end
+
+  def create
+    user_instrument = @current_user.user_instruments.build(user_instrument_params)
+    instrument = Instrument.find(params[:instrument_id])
+    instrument.user_instruments << user_instrument
+    user_instrument.save!
+    render json: user_instrument, status: :created
+  end
+
+  private
+  def user_instrument_params
+    params.permit(:instrument_id, :skill, :experience)
+  end
 end
