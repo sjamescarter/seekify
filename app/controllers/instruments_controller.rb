ed47cac4ab7600b30968e92d543f1837807c26cb
sjamescarter <stephenjamescarter@gmail.com>
2023-08-08 21:06:38 -0500

    add create action

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,4 +2,9 @@ class InstrumentsController < ApplicationController
   def index
     render json: Instrument.all
   end
+
+  def create
+    instrument = Instrument.create!(name: params[:name].strip.titlecase)
+    render json: instrument, status: :created
+  end
 end
