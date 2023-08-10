7ac457f64cf924399398c42239d6ef9760ca9fd5
sjamescarter <stephenjamescarter@gmail.com>
2023-08-10 16:37:27 -0500

    add name method

--- app/serializers/user_instrument_serializer.rb
+++ app/serializers/user_instrument_serializer.rb
@@ -1,5 +1,9 @@
 class UserInstrumentSerializer < ActiveModel::Serializer
-  attributes :id, :skill, :experience
+  attributes :id, :name, :skill, :experience
   has_one :user
   has_one :instrument
+
+  def name
+    object.instrument.name
+  end
 end
