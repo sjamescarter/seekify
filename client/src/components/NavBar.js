78401b8b00908c159183a855575c83a9d107c132
sjamescarter <stephenjamescarter@gmail.com>
2023-08-11 10:36:13 -0500

    update styles

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -22,8 +22,17 @@ function NavBar() {
         <Main>
             <NavGrid>
                 <h1>seekify.io</h1>
-                <input type='text' placeholder='Search Bar' />
-                <button onClick={handleLogout}>{user.name}</button>
+                {/* <input type='text' placeholder='Search Bar' /> */}
+                <Search className="material-symbols-outlined">search</Search>
+                <Avatar 
+                    src={user.profile.avatar 
+                        ? user.profile.avatar 
+                        : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.L-PLw9YL0s6ErCIcuprlKgAAAA%26pid%3DApi&f=1&ipt=98bca178f7faad18a400337a2735e92959f258e43128e375907f1e6d80f5b423&ipo=images" 
+                    } 
+                    alt="Avatar" 
+                    loading="lazy" 
+                    onClick={handleLogout} 
+                />
             </NavGrid>
         </Main>
     );
@@ -32,18 +41,39 @@ function NavBar() {
 const Main = styled.div`
     background-color: #8AA29E;
     color: white;
-    // position: fixed;
-    // top: 0;
+    position: fixed;
+    top: 0;
     width: 100%;
+    height: 75px;
 `
 
 const NavGrid = styled.div`
     align-items: center;
     display: grid;
-    grid-template-columns: 3fr 2fr 1fr;
+    grid-template-columns: auto 30px 50px;
+    grid-template-rows: 50px;
     padding: 10px;
     max-width: 800px;
+    min-width: 550px;
     margin: auto;
 `
-
+const Search = styled.i`
+    padding: 3px;
+    border-radius: 1em;
+    &:hover {
+        background-color: #3D5467;
+        cursor: pointer;
+    }
+`
+const Avatar = styled.img`
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    object-fit: cover;
+    object-position: 50%;
+    margin: auto;
+    &:hover {
+        border: 3px solid #3D5467;
+    }
+`
 export default NavBar;
