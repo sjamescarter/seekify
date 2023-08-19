56f587da75978117ea36cb79220b0b491226503f
sjamescarter <stephenjamescarter@gmail.com>
2023-08-19 18:12:25 -0500

    Update styles

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import FormItem from './FormItem';
 
 function Errors({ errors }) {
     return(
         <List>
-            {errors.map(err => <Item key={err}>{err}</Item>)}
+            {errors.map(err => 
+                <FormItem 
+                    key={err} 
+                    icon='error' 
+                    style={{color: "#DB5461"}}
+                >
+                    <Item>{err}</Item>
+                </FormItem>)
+            }
         </List>
     )
 }
@@ -11,6 +20,7 @@ function Errors({ errors }) {
 const List = styled.ul`
 `
 const Item = styled.li`
-    color: #DB5461
+    color: #DB5461;
+    list-style-type: none;
 `
 export default Errors;
