e9988ae739e3c851a922f40a0892074bba940150
sjamescarter <stephenjamescarter@gmail.com>
2023-08-19 18:00:46 -0500

    Add image method

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,11 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :date, :rehearsal, :public, :description
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :date, :rehearsal, :public, :description, :image
   has_one :host
   has_one :venue
+
+  def image
+    url_for(object.image) if object.image.attached?
+  end
 end
