d765d33f6bdf673f93e998ab2c540901d0ddbdad
sjamescarter <stephenjamescarter@gmail.com>
2023-08-21 16:35:44 -0500

    add location and time methods

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,11 +1,20 @@
 class EventSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :date, :rehearsal, :public, :description, :image
+  attributes :id, :name, :date, :time, :rehearsal, :public, :description, :image, :location
   has_one :host
   has_one :venue
+
+  def location
+    object.venue.name.upcase
+  end
 
   def image
     url_for(object.image) if object.image.attached?
   end
+
+  def time
+    object.date.to_fs(:small)
+  end
+
 end
