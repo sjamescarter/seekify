b3d5759a22bc8b2191a9f88604a3d8072685f8a9
sjamescarter <stephenjamescarter@gmail.com>
2023-08-22 11:49:47 -0500

    Add grid

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,20 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../context/user";
+import styled from "styled-components";
+
 function Dashboard() {
+    // Context
+    const { user } = useContext(UserContext);
+
     return (
-        <h1>Dashboard</h1>
+        <Grid>
+            <h1>Dashboard</h1>
+            <h2>Welcome back, {user.name}</h2>
+
+        </Grid>
     );
 }
 
+const Grid = styled.div`
+`
 export default Dashboard;
