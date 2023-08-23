350d3afd0cdd0ed3db6114bb2bf8004b590673d7
sjamescarter <stephenjamescarter@gmail.com>
2023-08-23 13:19:31 -0500

    Add validations

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,4 +1,8 @@
 class Invite < ApplicationRecord
   belongs_to :user_instrument
   belongs_to :event
+
+  validates :message, presence: true
+  validates :pay, numericality: { only_integer: true }, allow_nil: true
+  validates :status, inclusion: { in: %w(accepted pending declined) }
 end
