48ef1985d2b3188ccce5bab9ca64fdc7858d7dee
sjamescarter <stephenjamescarter@gmail.com>
2023-08-23 13:21:13 -0500

    Add create action

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,2 +1,14 @@
 class InvitesController < ApplicationController
+  def create
+    event = @current_user.events.find(params[:event_id])
+    invite = event.invites.create!(invite_params)
+    musician = UserInstrument.find(params[:user_instrument_id])
+    musician.invites << invite
+    render json: invite, status: :created
+  end
+
+  private
+  def invite_params
+    params.permit(:message, :pay, :status, :user_instrument_id)
+  end
 end
