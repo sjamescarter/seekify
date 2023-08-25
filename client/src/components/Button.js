42191ae72c600df09c60f353955684874f33d5a1
sjamescarter <stephenjamescarter@gmail.com>
2023-08-25 16:03:40 -0500

    Update styles

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,20 +1,25 @@
 import styled from 'styled-components';
 
 const main = {
-    border: "2px solid #8AA29E",
+    border: "1px solid #8AA29E",
     backgroundColor: "#8AA29E",
     color: "white"
 };
 const secondary = {    
     border: "1px solid #686963",
     backgroundColor: "white",
-    fontWeight: "600",
     color: "#686963"
 };
 const StyledButton = styled.button`
     border: none;
     border-radius: 10px;
     padding: 8px;
+    font-weight: 600;
+    opacity: .8;
+    &:hover {
+        opacity: 1;
+        cursor: pointer;
+    }
 `
 
 function Button({ onClick, children, alt=false }) {
