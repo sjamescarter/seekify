3e0ea150f23f359aef7673085b3467b8013f361a
sjamescarter <stephenjamescarter@gmail.com>
2023-08-25 16:05:45 -0500

    Rewrite for Dashboard

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,59 +1,49 @@
 import styled from "styled-components";
-import defaultEventPic from "../images/defaultEventPic.jpeg";
+import Button from "./Button";
+import { useState } from "react";
+import CreateInvite from "./CreateInvite";
 
 function EventCard({ event }) {
-    const { name, time, location, image, description } = event
-    
-    const eventImage = image ? image : defaultEventPic
+    const { name, time } = event;
+    const [expand, setExpand] = useState();
+    const [invite, setInvite] = useState();
 
     return(
-        <Container>
-            <Img src={eventImage} alt="Event cover"/>
-            <div></div>
-            <Title>{name}</Title>
-            <Strong>{time} | {location}</Strong>
-            <Description>{description}</Description>
-        </Container>
+        <Div>
+            <h2 onClick={() => setExpand(!expand)}>{name}</h2>
+            <small>{time}</small>
+            { expand
+                ? <>
+                    <div>
+                        <Button>Edit Event</Button>
+                        <Button onClick={() => setInvite(!invite)}>Add Role</Button>
+                    </div>
+                    <div>
+                    <h3>Roles</h3>
+                    <ul>
+                        {event.roles.map(r => <li key={r.id}>{r.user_instrument.name} :: {r.role} :: {r.status.toUpperCase()}</li>)}
+                    </ul>
+                    </div>
+                </>
+                : null
+            }
+            { invite
+                ? <CreateInvite event={event} setInvite={setInvite} />
+                : null
+            }
+        </Div>
     );
 }
 
-// Styles
-const Container = styled.div`
-    display: grid;
+const Div = styled.div`
     background-color: #F1EDEE;
-    color: #686963;
-    grid-template-columns: 200px auto;
-    grid-template-rows: 35px 30px 35px 1fr;
-    height: 200px;
-    // min-width: 550px;
-    border-top-right-radius: 24px;
-    border-top-left-radius: 12px;
-    border-bottom-left-radius: 24px;
-    margin: 2em 0;
-    text-align: right;
-
-`
-const Img = styled.img`
-    background-color: #3D5467;
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-    object-position: 50%;
-    grid-row: 1 / span 4;
-    // border-top-left-radius: 12px;
-    border-bottom-left-radius: 24px;
-`
-const Title = styled.h2`
-    color: #3D5467;
-    margin: 0 15px;
-    line-height: 1;
-`
-const Description = styled.p`
-    padding: 0 15px;
-`
-const Strong = styled.strong`
-    padding: 2px 15px;
-    font-size: .8em;
-    border-top: .5px solid #686963;
+    border-radius: 10px;
+    display: grid;
+    grid-template-rows: 40px 30px 1fr;
+    gap: 10px;
+    margin: auto;
+    margin-bottom: 15px;
+    padding: 20px;
+    max-width: 500px;
 `
 export default EventCard;
