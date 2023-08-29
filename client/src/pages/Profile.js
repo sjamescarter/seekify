8cff13fcb317b881bb1c1307a8c925ce19bd7caf
sjamescarter <stephenjamescarter@gmail.com>
2023-08-29 11:48:23 -0500

    Add buttons

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,13 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { abc, addS, chron } from '../components/utilities';
+import Button from '../components/Button';
 import PublicEventCard from '../components/PublicEventCard';
 
 function Profile({ user }) {
     const { profile, church, user_instruments, events } = user
+    const navigate = useNavigate();
     const currentEvents = events.filter(event => {
         const today = Date.now()
         const date = new Date(event.date)
@@ -12,16 +15,19 @@ function Profile({ user }) {
     })
 
     // State
-    const [editProfile, setEditProfile] = useState(false);
+    // const [editProfile, setEditProfile] = useState(false);
 
     return(
         <ProfileGrid>
             { <Avatar src={profile.avatar ? profile.avatar : "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.L-PLw9YL0s6ErCIcuprlKgAAAA%26pid%3DApi&f=1&ipt=98bca178f7faad18a400337a2735e92959f258e43128e375907f1e6d80f5b423&ipo=images" } alt="Avatar" loading="lazy" /> }
             <div></div>
             <Name>{user.name}</Name>
-            <Location>{profile.city}, {profile.state} | {church.name}</Location>
+            <Location>{profile.city}, {profile.state} :: {church.name}</Location>
             <div></div>
-            <Button>{editProfile ? "Save Profile" : "Edit Profile"}</Button>
+            <Button>
+                <span className="material-symbols-rounded">edit</span>
+                Edit Profile
+            </Button>
             <h6>My Connections</h6>
             <Div>
                 <h3>Bio</h3>
@@ -29,7 +35,11 @@ function Profile({ user }) {
             </Div>
             <Div>
                 <h3>Instruments</h3>
-                {abc(user_instruments).map(i => <li key={i.id}>{i.instrument} | {i.skill} | {i.experience} year{addS(i.experience)} experience</li>)}
+                {abc(user_instruments).map(i => <li key={i.id}>{i.instrument} :: {i.skill} :: {i.experience} year{addS(i.experience)} experience</li>)}
+                <Button onClick={() => navigate('/profile/add-instrument')}>
+                    <span class="material-symbols-rounded">add_circle</span>
+                    Add Instrument
+                </Button>
             </Div>
             <Div>
                 <h3>Videos</h3>
@@ -56,6 +66,7 @@ const ProfileGrid = styled.div`
     justify-content: center;
     padding: 1em;
     margin: auto;
+    margin-bottom: 50px;
     max-width: 800px;
     min-width: 550px;
 `
@@ -77,20 +88,21 @@ const Name = styled.h1`
 const Location = styled.small`
     // grid-row: 3 / span 4;
 `
-const Button = styled.button`
-    background-color: #8AA29E;
-    border: none;
-    border-radius: 2em;
-    color: white;
-    opacity: .75;
-    margin: auto;
-    padding: 10px;
-    width: 12em;
-    &:hover {
-        cursor: pointer;
-        opacity: 1;
-    }
-`
+// const Button = styled.button`
+//     background-color: #8AA29E;
+//     border: none;
+//     border-radius: 10px;
+//     display: grid;
+//     grid-template-columns: 30px 1fr 15px;
+//     align-items: center;
+//     color: white;
+//     margin: auto;
+//     padding: 10px;
+//     width: 12em;
+//     &:hover {
+//         cursor: pointer;
+//     }
+// `
 const Div = styled.div`
     grid-column: 1 / span 2;
     margin: 1em;
