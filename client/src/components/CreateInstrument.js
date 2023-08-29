0114573b6627d817ab0aba938dd28d41a5e6727f
sjamescarter <stephenjamescarter@gmail.com>
2023-08-29 11:37:21 -0500

    Refactor, add autofocus

--- client/src/components/CreateInstrument.js
+++ client/src/components/CreateInstrument.js
@@ -1,15 +1,15 @@
 import { useContext, useState } from "react";
-import { UserContext } from '../context/user';
+import { InstrumentsContext } from "../context/instruments";
+import { Input } from '../styles'
 import { handleChange } from './utilities';
 import Form from "./Form";
-import { Input } from '../styles'
 import FormItem from "./FormItem";
 
 const formFields = { name: "" };
 
 function CreateInstrument({state, setState}) {
     // Context
-    const { instruments, setInstruments } = useContext(UserContext);
+    const { instruments, setInstruments } = useContext(InstrumentsContext);
 
     // State
     const [form, setForm] = useState(formFields);
@@ -56,6 +56,7 @@ function CreateInstrument({state, setState}) {
                         placeholder="Instrument Name" 
                         value={form.name} 
                         onChange={onChange} 
+                        autoFocus
                     />
                 </FormItem>
             </Form>
