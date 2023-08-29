365dd6479615f6c36122fa3b72a1525a377de086
sjamescarter <stephenjamescarter@gmail.com>
2023-08-29 11:25:41 -0500

    Add update and destroy actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,8 +11,25 @@ class EventsController < ApplicationController
     render json: event, status: :created
   end
 
+  def update
+    event = find_event
+    event.update!(event_params)
+    event.image.attach(event_params[:image]) unless event_params[:image].nil?
+    render json: event, status: :accepted
+  end
+
+  def destroy
+    event = find_event
+    event.destroy
+    head :no_content
+  end
+
   private
   def event_params
     params.permit(:name, :date, :rehearsal, :image, :description, :public, :venue_id)
+  end
+
+  def find_event
+    @current_user.events.find(params[:id])
   end
 end
