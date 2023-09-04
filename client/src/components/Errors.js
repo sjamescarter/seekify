f09cc3ac29e5039189a9f2805f10d062f75d5ffc
sjamescarter <stephenjamescarter@gmail.com>
2023-09-04 09:53:59 -0500

    Update styles

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -4,6 +4,7 @@ import FormItem from './FormItem';
 function Errors({ errors }) {
     return(
         <List>
+            <Legend>Errors</Legend>
             {errors.map(err => 
                 <FormItem 
                     key={err} 
@@ -17,7 +18,14 @@ function Errors({ errors }) {
     )
 }
 
-const List = styled.ul`
+const List = styled.fieldset`
+    border: 2px solid #DB5461;
+    border-radius: 10px;
+    background-color: #FBEEEF;
+    font-weight: 600;
+`
+const Legend = styled.legend`
+    color: #DB5461;
 `
 const Item = styled.li`
     color: #DB5461;
