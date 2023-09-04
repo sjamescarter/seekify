7be46547b6288f4a891414ddedb8a70d22ce984e
sjamescarter <stephenjamescarter@gmail.com>
2023-09-04 10:35:07 -0500

    Refactor

--- client/src/pages/People.js
+++ client/src/pages/People.js
@@ -4,11 +4,12 @@ import Profile from "./Profile";
 
 function People() {
     const { musicians } = useContext(MusiciansContext);
-console.log(musicians)
+
     if(!musicians) return <h1>Loading...</h1>
+
     return (
         <div>
-            {musicians.map(person => person.profile ? <Profile user={person} /> : null)}
+            {musicians.map(person => person.profile ? <Profile key={person.id} user={person} /> : null)}
         </div>
     );
 }
