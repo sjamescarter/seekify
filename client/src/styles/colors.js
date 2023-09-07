0a1b54351960ef003c2facafedd0f5a56cd4ec18
sjamescarter <stephenjamescarter@gmail.com>
2023-09-07 15:46:16 -0500

    Create colors.js

---
+++ client/src/styles/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    main: "#8AA29E", // Cambridge Blue
+    mainHover: "#77928E", // 2 shades darker
+    secondary: "#3D5467", // Charcoal
+    secondaryHover: "#2D3F4D", // 2 shades darker
+    gray: "#686963", // Dim Gray
+    nuetral: "#F1EDEE", // White Smoke
+    red: "#DB5461", // Indian Red
+    redHover: "#D43545" // 2 shades darker
+}
+
+export { colors };
