ee10b5c542aab6a9310a5114eaa3057c01c447dd
sjamescarter <stephenjamescarter@gmail.com>
2023-09-13 09:33:13 -0500

    Add destroy action

--- app/controllers/user_instruments_controller.rb
+++ app/controllers/user_instruments_controller.rb
@@ -11,6 +11,12 @@ class UserInstrumentsController < ApplicationController
     render json: user_instrument, status: :created
   end
 
+  def destroy
+    instrument = @current_user.user_instruments.find(params[:id])
+    instrument.destroy
+    head :no_content
+  end
+  
   private
   def user_instrument_params
     params.permit(:instrument_id, :skill, :experience)
