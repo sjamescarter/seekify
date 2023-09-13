0fe50b0672ad55e92d8b5a88d17318fec769f873
sjamescarter <stephenjamescarter@gmail.com>
2023-09-13 09:45:06 -0500

    Update with new files

--- client/src/styles/index.js
+++ client/src/styles/index.js
@@ -1,5 +1,8 @@
+import { colors } from "./colors"
 import Input from "./Input";
 import Select from "./Select";
+import TableHeader from "./TableHeader";
+import TableRow from "./TableRow";
 import TextArea from "./TextArea";
 
-export { Input, Select, TextArea };
+export { colors, Input, Select, TableHeader, TableRow, TextArea };
