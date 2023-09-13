ff22fd10a7c5c62d7752a173e093343d43dab2c8
sjamescarter <stephenjamescarter@gmail.com>
2023-09-13 09:33:54 -0500

    Destroy dependent

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :venue
-  has_one_attached :image
-  has_many :invites
+  has_one_attached :image, dependent: :destroy
+  has_many :invites, dependent: :destroy
   alias :roles :invites
   alias :host :user
   
