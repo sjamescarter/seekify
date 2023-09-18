9413800f9e0bbb2dbe2f619e2e92a32af575a322
sjamescarter <stephenjamescarter@gmail.com>
2023-09-18 13:17:44 -0500

    Move Button component

---
+++ client/src/styles/Button.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { colors } from '.';
+
+const Button = styled.button`
+    border: none;
+    border-radius: 10px;
+    color: white;
+    background-color: ${colors.main};
+    padding: 8px;
+    margin: 10px;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+        background-color: ${colors.mainHover};
+        cursor: pointer;
+    }
+`
+export default Button;
