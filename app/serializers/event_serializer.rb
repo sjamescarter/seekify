d25d84cb6eeba9804ef61f0cecdb0ab11a27a432
sjamescarter <stephenjamescarter@gmail.com>
2023-09-22 15:04:42 -0500

    Add rehearsal_time

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,7 +1,7 @@
 class EventSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :date, :time, :rehearsal, :public, :description, :image, :location
+  attributes :id, :name, :date, :time, :rehearsal, :rehearsal_time, :public, :description, :image, :location
   has_many :roles
   belongs_to :venue
 
@@ -9,20 +9,23 @@ class EventSerializer < ActiveModel::Serializer
     object.date.to_fs(:fe)
   end
 
-  def location
-    object.venue.name.upcase
-  end
-
   def image
     url_for(object.image) if object.image.attached?
   end
 
-  def time
-    object.date.to_fs(:small)
+  def location
+    object.venue.name.upcase
   end
 
   def rehearsal
     object.rehearsal.to_fs(:fe) unless object.rehearsal.nil?
   end
+  
+  def rehearsal_time
+    object.rehearsal.to_fs(:small) unless object.rehearsal.nil?
+  end
 
+  def time
+    object.date.to_fs(:small)
+  end
 end
