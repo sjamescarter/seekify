ebb30412d59c375b04fd30382c79a1c0eced1090
sjamescarter <stephenjamescarter@gmail.com>
2023-09-22 15:21:16 -0500

    Refactor, add styles

--- client/src/components/InviteCard.js
+++ client/src/components/InviteCard.js
@@ -2,13 +2,19 @@ import { useContext, useState } from 'react';
 import { UserContext } from '../context/user';
 import styled from 'styled-components';
 import { colors } from '../styles';
+import PublicEventCard from './PublicEventCard';
+import Icon from './Icon';
 
 function InviteCard({ invite }) {
+    // Context
     const { user, setUser } = useContext(UserContext)
-    const { id, message, pay, event, role, host, venue, status } = invite
-    const { name, time, rehearsal } = event
-    const [show, setShow] = useState()
+    const { id, message, pay, event, role, host, status } = invite
+    const { rehearsal_time } = event
 
+    // State
+    const [expand, setExpand] = useState()
+
+    // Handlers
     function handleSubmit(e) {
         e.preventDefault();
         fetch(`/invites/${invite.id}`, {
@@ -39,95 +45,121 @@ function InviteCard({ invite }) {
                     )
                 ]
             });
-            setShow();
+            setExpand();
         })
     }
 
     return (
-        <Div>
-            <Title 
-                style={{ 
-                    gridColumn: "1 / span 2",
-                    color: status === "pending" ? colors.secondary : status === "accepted" ? colors.main : colors.red
-                }} 
-                onClick={() => setShow(!show)}>{status.toUpperCase()} request for {time}
-            </Title>
-            { show 
-                ?<>
-                    <Item>
-                        <h4>Event</h4>
-                        <p>{name}</p> 
-                    </Item>
-                    <Item>
-                        <h4>Date</h4> 
-                        <p>{time}</p>
-                    </Item>
-                    <Item>
-                        <h4>Host</h4>
-                        <p>{host}</p>
-                    </Item>
-                    <Item>
-                        <h4>Location</h4>
-                        <p>{venue}</p>
-                    </Item>
-                    <Item>
-                        <h4>Role</h4>
-                        <p>{role}</p>
-                    </Item>
-                    <Item>
-                        <h4>Rehearsal Time</h4>
-                        <p>{rehearsal}</p>
-                    </Item>
-                    { pay 
-                        ?<Item>
-                            <h4>Pay</h4>
-                            <p>${pay}</p>
-                        </Item>
+        <>
+            <div>
+                <Banner 
+                    style={{ 
+                        backgroundColor: 
+                            status === "pending" 
+                                ? colors.secondary 
+                                : status === "accepted" 
+                                    ? colors.main 
+                                    : colors.red
+                    }} 
+                >
+                    <Icon>                    
+                        { status === "pending" 
+                        ? "pending" 
+                        : status === "accepted" 
+                        ? "check_circle" 
+                        : "cancel"
+                        }
+                    </Icon>
+                    {role} invite {status}
+                </Banner>
+                <PublicEventCard event={event} onClick={() => setExpand(!expand)}>
+                    { pay ? <Icon title={`Paid Gig: $${pay}`}>paid</Icon> : <div></div> }
+                    { expand 
+                        ?<Container>
+                            <Item>
+                                <H4>HOST</H4>
+                                <P>{host}</P>
+                            </Item>
+                            <Item>
+                                <H4>ROLE</H4>
+                                <P>{role}</P>
+                            </Item>                           
+                            <Item
+                                style={{gridColumn: pay ? "" : "3 / span 4"}}
+                            >
+                                <H4>REHEARSAL</H4>
+                                <P>{rehearsal_time ? rehearsal_time : "None"}</P>
+                            </Item>
+                            { pay 
+                                ?<Item>
+                                    <H4>PAY</H4>
+                                    <P>${pay}</P>
+                                </Item>
+                                : <div></div>
+                            }
+                            <Item style={{gridColumn: "1 / span 4"}}>
+                                <H4>MESSAGE</H4>
+                                <P>{message}</P>
+                            </Item>
+                            <form 
+                                id='accepted' 
+                                style={{gridColumn: "1 / span 2"}} 
+                                onSubmit={handleSubmit}
+                            >
+                                <Accept type="submit" value="Accept" />
+                            </form>
+                            <form 
+                                id='declined' 
+                                style={{gridColumn: "3 / span 4"}} 
+                                onSubmit={handleSubmit}
+                            >
+                                <Decline type="submit" value="Decline" />
+                            </form>
+                        </Container>
                         : null
                     }
-                    <Item style={{gridColumn: "1 / span 2"}}>
-                        <h4>Message</h4>
-                        <p>{message}</p>
-                    </Item>
-                    <form id='accepted' onSubmit={handleSubmit}>
-                        <Accept type="submit" value="Accept" />
-                    </form>
-                    <form id='declined' onSubmit={handleSubmit}>
-                        <Decline type="submit" value="Decline" />
-                    </form>
-                </>
-                : null
-            }
-        </Div>
+                </PublicEventCard>
+            </div>
+        </>
     );
 }
 
-const Div = styled.div`
-    background-color: ${colors.nuetral};
-    border-radius: 10px;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 10px;
-    margin: auto;
-    margin-bottom: 15px;
-    padding: 20px;
-    &:hover{
-        cursor: pointer;
-        box-shadow: 0 5px 16.83px 0.17px rgba(0, 0, 0, 0.25);
-    }
-`
-const Title = styled.h2`
+// Styles
+const Banner = styled.div`
+    display: flex;
+    // justify-content: space-between;
+    align-items: center;
     text-align: center;
-    color: #3D5467;
+    font-size: 24px;
+    color: white;
+    margin: 0 16px;
+    padding: 3px 16px;
+    width: 80%;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+`
+const Container = styled.div`
+    background-color: ${colors.nuetral};
+    display: grid;
+    grid-template-columns: 1fr 1fr 1.5fr .5fr;
+    gap: 10px;
+    margin: 2px 0 0 30px;
+    margin-bottom: 15px;
+    border-bottom-right-radius: 12px;
+    padding: 20px;
 `
 const Item = styled.div`
     display: grid;
-    grid-template-rows: 25px 30px;
+    grid-template-rows: 25px 1fr;
 `
-const Accept = styled.input`
-    margin: auto;
+const H4 = styled.h4`
+    color: ${colors.gray}
+`
+const P = styled.p`
+    font-size: 1.2em;
+`
+const Submit = styled.input`
     width: 100%;
-    background-color: ${colors.main};
     color: white;
     border: none;
     border-radius: 10px;
@@ -137,25 +169,19 @@ const Accept = styled.input`
     font-size: 1.3em;
     font-weight: 800;
     &:hover {
-        background-color: ${colors.mainHover};
         cursor: pointer;
     }
 `
-const Decline = styled.input`
-    margin: auto;
-    width: 100%;
+const Accept = styled(Submit)`
+    background-color: ${colors.main};
+    &:hover {
+        background-color: ${colors.mainHover};
+    }
+`
+const Decline = styled(Submit)`
     background-color: ${colors.red};
-    color: white;
-    border: none;
-    border-radius: 10px;
-    padding: 10px;
-    margin: 12px 0;
-    font: inherit;
-    font-size: 1.3em;
-    font-weight: 800;
     &:hover {
         background-color: ${colors.redHover};
-        cursor: pointer;
     }
 `
 export default InviteCard;
