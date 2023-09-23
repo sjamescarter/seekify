bfb7d26667ec39bf7565772e1b20898746a36c4e
sjamescarter <stephenjamescarter@gmail.com>
2023-09-23 17:34:46 -0500

    Refactor

--- client/src/pages/People.js
+++ client/src/pages/People.js
@@ -1,24 +1,30 @@
 import { useContext } from "react";
-import { MusiciansContext } from "../context/musicians";
-import Profile from "./Profile";
 import { useParams } from "react-router-dom";
+import { MusiciansContext } from "../context/musicians";
+import { UserContext } from "../context/user";
+import Profile from "../components/Profile";
+import { abc } from "../components/utilities";
 
 function People() {
+    // Context
     const { musicians } = useContext(MusiciansContext);
+    const { user } = useContext(UserContext);
     const { id } = useParams();
     
     if(!musicians) return <h1>Loading...</h1>;
 
+    // Constants
     const musician = musicians.find((person) => person.id === parseInt(id));
+    const filtered = [...musicians].filter(m => m.id !== user.id); // Remove current user
+    const people = abc([...filtered]);
 
     return (
         <>
             { musician
                 ? <Profile person={musician} />
-                : musicians.map(person => person.profile 
-                    ? <Profile key={person.id} person={person} /> 
-                    : null
-            )}
+                : people.map(person => <Profile key={person.id} person={person} /> 
+                )
+            }
         </>
     );
 }
