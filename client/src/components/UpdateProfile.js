138b6139506c1610f81a6854e2a1c6cb23cdcf4f
sjamescarter <stephenjamescarter@gmail.com>
2023-09-25 08:47:16 -0500

    Create UpdateProfile component and refactor

---
+++ client/src/components/UpdateProfile.js
@@ -0,0 +1,97 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MusiciansContext } from '../context/musicians';
+import { UserContext } from '../context/user';
+import { VenuesContext } from '../context/venues';
+import { handleModal } from '../components/utilities';
+import { handleImgSubmit } from '../components/utilities';
+import CreateVenue from '../components/CreateVenue';
+import Modal from '../components/Modal';
+import ProfileForm from '../components/ProfileForm';
+
+function UpdateProfile() {
+    // Context
+    const { musicians, setMusicians } = useContext(MusiciansContext);
+    const { user, setUser } = useContext(UserContext);
+    const { venues } = useContext(VenuesContext);
+    const { profile, church, name } = user;
+    const { id, phone, city, state, bio, video_url } = profile;
+
+    // State
+    const [form, setForm] = useState({
+        firstName: name.split(" ")[0], 
+        lastName: name.split(" ")[1], 
+        phone: phone, 
+        city: city, 
+        state: state, 
+        bio: bio, 
+        venueId: church.id, 
+        videoUrl: video_url
+    });
+    const [img, setImg] = useState();
+    const [errors, setErrors] = useState();
+    
+    // Const
+    const endpoint = `/profiles/${id}`;
+    const method = "PATCH";
+    const imgLabel = 'avatar';
+    const modalId = `createProfileVenue${id}`;
+
+    const navigate = useNavigate();
+    const callback = (data) => {
+        setUser({
+            ...user,
+            name: `${data.first_name} ${data.last_name}`,
+            profile: data
+        });
+        setMusicians([
+            ...musicians.map(m => m.id === user.id
+                ? {
+                    ...m,
+                    name: `${data.first_name} ${data.last_name}`,
+                    profile: data
+                }
+                : m
+            )
+        ]);
+        navigate(`/people/${user.id}`);
+    };
+
+    // Handlers
+    const handleCancel = () => {
+        handleModal(modalId);
+        setForm({ ...form, venueId: "" });
+    }
+    const onSubmit = (e) => handleImgSubmit(e, endpoint, method, setErrors, form, imgLabel, img, callback)
+
+    if(form.venueId === "new") { 
+        handleModal(modalId, true);
+    };
+
+    return (
+        <>
+            <ProfileForm 
+                form={form}
+                setForm={setForm}
+                title="Settings"
+                onSubmit={onSubmit}
+                errors={errors}
+                venues={venues}
+                onCancel={() => navigate(`/people/${user.id}`)}
+                imgLabel={imgLabel}
+                img={img}
+                setImg={setImg}
+            />
+            <Modal id={modalId}>
+                <CreateVenue 
+                    state={form} 
+                    setState={setForm} 
+                    handleCancel={handleCancel} 
+                    closeModal={() => handleModal(modalId)}
+                />
+            </Modal>
+        </>
+    );
+}
+
+export default UpdateProfile;
--- client/src/pages/CreateProfile.js
+++ client/src/pages/CreateProfile.js
@@ -2,15 +2,12 @@ import { useContext, useState } from 'react';
 import { MusiciansContext } from '../context/musicians';
 import { UserContext } from '../context/user';
 import { VenuesContext } from '../context/venues';
-import { states, handleChange, camelToTitle, handleModal } from '../components/utilities';
-import { Input, Select, TextArea } from '../styles';
+import { handleModal } from '../components/utilities';
 import { handleImgSubmit } from '../components/utilities';
 import CreateVenue from '../components/CreateVenue';
-import Form from '../components/Form'
-import FormItem from '../components/FormItem';
-import ImgUploader from '../components/ImgUploader';
 import Modal from '../components/Modal';
-import VenueSelect from '../components/VenueSelect';
+import ProfileForm from '../components/ProfileForm';
+import { useNavigate } from 'react-router-dom';
 
 const formFields = {
     firstName: "", 
@@ -35,20 +32,22 @@ function CreateProfile({ handleLogout }) {
     const [errors, setErrors] = useState();
     
     // Const
+    const navigate = useNavigate();
     const callback = (data) => {
         setUser(data);
         setMusicians([...musicians, data]);
+        navigate(`/people/${data.id}`)
     };
-    const endpoint = 'profiles';
+    const endpoint = '/profiles';
+    const method = "POST";
     const imgLabel = 'avatar';
 
     // Handlers
-    const onChange = (e) => handleChange(e, form, setForm);
     const handleCancel = () => {
-        handleModal('createPrrofileVenue');
+        handleModal('createProfileVenue');
         setForm({ ...form, venueId: "" });
     }
-    const onSubmit = (e) => handleImgSubmit(e, endpoint, setErrors, form, imgLabel, img, callback);
+    const onSubmit = (e) => handleImgSubmit(e, endpoint, method, setErrors, form, imgLabel, img, callback);
 
     if(form.venueId === "new") { 
         handleModal('createProfileVenue', true);
@@ -56,76 +55,18 @@ function CreateProfile({ handleLogout }) {
 
     return (
         <>
-            <Form 
-                title='Create Profile'
-                onSubmit={onSubmit} 
-                errors={errors} 
-                handleCancel={handleLogout}
-            >
-                <FormItem icon="person">
-                    {Object.keys(form).slice(0, 2).map(key => 
-                        <Input 
-                            key={key} 
-                            type="text"
-                            name={key}
-                            placeholder={camelToTitle(key)}
-                            value={form[key]}
-                            onChange={onChange}
-                        />
-                    )}
-                </FormItem>
-                <FormItem icon="phone">
-                    <Input 
-                        type="tel" 
-                        name="phone"
-                        placeholder='888-888-8888'
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        value={form.phone} 
-                        onChange={onChange} 
-                    />
-                </FormItem>
-                <FormItem icon='location_on'>
-                    <Input 
-                        type="text" 
-                        name="city" 
-                        placeholder='City' 
-                        value={form.city} 
-                        onChange={onChange}
-                    />
-                    <Select 
-                        name="state" 
-                        value={form.state} 
-                        onChange={onChange} 
-                        >
-                        <option>State</option>
-                        {states.map(state => <option key={state} value={state}>{state}</option>)}
-                    </Select>
-                </FormItem>
-                <FormItem icon='description'>
-                    <TextArea 
-                        name='bio' 
-                        placeholder='Tell us about yourself...' 
-                        rows='5'
-                        value={form.bio} 
-                        onChange={onChange} 
-                    />
-                </FormItem>
-                <FormItem icon="videocam">
-                    <Input 
-                        type="url" 
-                        name="videoUrl" 
-                        placeholder='Video URL: https://www.youtube.com/' 
-                        value={form.videoUrl} 
-                        onChange={onChange} 
-                    />
-                </FormItem>
-                <VenueSelect onChange={onChange} value={form.venueId} venues={venues} />
-                <ImgUploader 
-                    id={imgLabel}
-                    img={img}
-                    setImg={setImg}
-                />            
-            </Form>
+            <ProfileForm 
+                form={form}
+                setForm={setForm}
+                title="Create Profile"
+                onSubmit={onSubmit}
+                errors={errors}
+                venues={venues}
+                onCancel={handleLogout}
+                imgLabel={imgLabel}
+                img={img}
+                setImg={setImg}
+            />
             <Modal id="createProfileVenue">
                 <CreateVenue 
                     state={form} 
