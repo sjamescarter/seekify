ce97c608977226958838edec8e3e9a36f2bb3159
sjamescarter <stephenjamescarter@gmail.com>
2024-01-16 11:07:10 -0600

    Prep for depolyment

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  resources :events, only: [:index, :create, :update, :destroy] do
-    resources :invites, only: [:create, :destroy]
-  end
-  resources :invites, only: [:update]
-  resources :instruments, only: [:index, :create]
-  resources :profiles, only: [:index, :create, :update]
-  resources :user_instruments, only: [:index, :create, :update, :destroy]
-  resources :users, only: [:index, :destroy]
-  resources :venues, only: [:index, :create, :show]
+  namespace :api do
+    resources :events, only: [:index, :create, :update, :destroy] do
+      resources :invites, only: [:create, :destroy]
+    end
+    resources :invites, only: [:update]
+    resources :instruments, only: [:index, :create]
+    resources :profiles, only: [:index, :create, :update]
+    resources :user_instruments, only: [:index, :create, :update, :destroy]
+    resources :users, only: [:index, :destroy]
+    resources :venues, only: [:index, :create, :show]
 
-  get '/me', to: 'users#show'
-  post '/signup', to: 'users#create'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    get '/me', to: 'users#show'
+    post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end
   
   get '*path',
     to: 'fallback#index',
